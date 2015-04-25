673c78219bd04a586d5494c455a0419adbb70d6a
xohozu <xohozu@gmail.com>
2015-04-25 16:58:24 +0800

    Improved #203 Remove Linked List Elements
    增加一个dummy节点，使两种情况统一为一种形式。
    ---------------------------------------------------------------------
    主要考虑两种情况：
    1 删除链表头节点（可能连续）
    2 中间及尾节点（同样注意连续删除的情况）

--- 203 Remove Linked List Elements/Solution.h
+++ 203 Remove Linked List Elements/Solution.h
@@ -19,26 +19,39 @@ struct ListNode {
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        // the value of head node equals `val`
-        // loop check
-        while (head && head->val == val) {
-            ListNode *t = head;
-            head = head->next;
-            delete t;
-        }
-        if (!head) return head;
-        ListNode *next = head->next, *prev = head;
-        while (next) {
-            if (next->val == val) {
-                // remove intermediate element
-                prev->next = next->next;
-                delete next;
-                next = prev;
+//        // the value of head node equals `val`
+//        // loop check
+//        while (head && head->val == val) {
+//            ListNode *t = head;
+//            head = head->next;
+//            delete t;
+//        }
+//        if (!head) return head;
+//        ListNode *next = head->next, *prev = head;
+//        while (next) {
+//            if (next->val == val) {
+//                // remove intermediate element
+//                prev->next = next->next;
+//                delete next;
+//                next = prev;
+//            }
+//            prev = next;
+//            next = next->next;
+//        }
+//        return head;
+        ListNode *dummy = new ListNode(-1), *prev;
+        dummy->next = head;
+        prev = dummy;
+        while (head) {
+            if (head->val == val) {
+                prev->next = head->next;
+                delete head;
+                head = prev;
             }
-            prev = next;
-            next = next->next;
+            prev = head;
+            head = prev->next;
         }
-        return head;
+        return dummy->next;
     }
 };
 
